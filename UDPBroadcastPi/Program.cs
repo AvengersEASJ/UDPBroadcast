a8daa91655799caceaf7fd5e190b9cbfc14518df
mads118 <vpmads@gmail.com>
2018-12-11 13:38:08 +0100

    tilføjet 3rd party api

--- UDPBroadcastPi/Program.cs
+++ UDPBroadcastPi/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 using System.Threading;
 
 namespace UDPNumberSenderBroad
@@ -16,15 +18,15 @@ namespace UDPNumberSenderBroad
             const string conn = "Server=tcp:frienddb.database.windows.net,1433;Initial Catalog=FriendDatabase;Persist Security Info=False;User ID=avengers;Password=Mads12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
             UdpClient udpClient = new UdpClient(0);
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Broadcast, 9999);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Broadcast, 4000);
 
-
+           
 
 
             while (true)
             {
 
-                string sql = "SELECT * FROM FRIENDS WHERE friendsID = 1";
+                string sql = "SELECT * FROM FRIENDS WHERE friendsID = 4";
 
                 using (var databaseConnection = new SqlConnection(conn))
                 {
@@ -39,21 +41,30 @@ namespace UDPNumberSenderBroad
                             {
                                 while (reader.Read())
                                 {
+                                    WebClient webClient = new WebClient();
+                                    string reply = webClient.DownloadString($"http://api.openweathermap.org/data/2.5/weather?q=Roskilde&APPID=2f16a2d58ddb4a816fe93d7520731870");
+                                    var kelvinTemp = JObject.Parse(reply)["main"]["temp"];
+                                    int grader = (int)kelvinTemp - 273;
 
-                                    int friendsID = reader.GetInt32(0);
                                     string friendsName = reader.GetString(1);
+                                    int friendsID = reader.GetInt32(0);                                    
                                     bool gender = reader.GetBoolean(2);
                                     int thirst = reader.GetInt32(3);
                                     int hunger = reader.GetInt32(4);
                                     int task = reader.GetInt32(5);
                                     int fun = reader.GetInt32(6);
+                                    int degrees = grader;
                                     int dress = reader.GetInt32(7);
+                                    
 
                                     Byte[] sendBytes = Encoding.ASCII.GetBytes(friendsID + " " + friendsName + " " + gender + " " + thirst + " " + hunger +
-                                                                               " " + task + " " + fun + " " + dress);
+                                                                               " " + task + " " + fun + " " + " " + degrees + " " + dress);
                                     udpClient.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint);
                                     Console.Write("Message is: " + friendsID + " " + friendsName + " " + gender + " " + thirst + " " + hunger +
-                                                                    " " + task + " " + fun + " " + dress + "\n");
+                                                                    " " + task + " " + fun + " " + " " + degrees + " " + dress + "\n");
+
+                                    
+                                
                                     Thread.Sleep(1000);
 
                                 }
